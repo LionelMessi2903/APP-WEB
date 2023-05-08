0d9fb1043f56714665495a73d5d9560c057f177e
LionelMessi2903 <adribanano2904@gmail.com>
2023-05-07 20:51:07 -0500

    Actualizado repositorio

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { Error404Component } from './notfound/error404/error404.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { AboutComponent } from './web/about/about.component';
+import { ContactsComponent } from './web/contacts/contacts.component';
+import { HomeComponent } from './web/home/home.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+{path: '', redirectTo: 'login', pathMatch:'full'},
+{path: 'login', component:LoginComponent},
+{path: 'register', component:RegisterComponent},
+{path: 'error404', component:Error404Component},
+{path: '**', redirectTo: 'error404'},
+{path: 'category', component:CategoryComponent},
+{path: 'dashboard', component:DashboardComponent},
+{path: 'products', component:ProductsComponent},
+{path: 'about', component:AboutComponent},
+{path: 'contacts', component:ContactsComponent},
+{path: 'home', component:HomeComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { BreadcrumbsModule } from './breadcrumbs/breadcrumbs.module';
+import { NotfoundModule } from './notfound/notfound.module';
+import { PagesModule } from './pages/pages.module';
+import { SharedModule } from './shared/shared.module';
+import { WebModule } from './web/web.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AuthModule,
+    PagesModule,
+    WebModule,
+    SharedModule,
+    NotfoundModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
